acb1bd923b8f39db1077eb4712c1155ab8344945
HasnainArifYU <harif@pgh.yearup.org>
2024-04-25 10:20:19 -0400

    final

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,2 +1,69 @@
-package com.pluralsight;public class Book {
+package com.pluralsight;
+
+public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
+
+
+    public Book(int id, String checkedOutTo, String isbn, String title, boolean isCheckedOut) {
+        this.id = id;
+        this.checkedOutTo = checkedOutTo;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name) {
+        this.checkedOutTo=name;
+        this.isCheckedOut=true;
+    }
+
+    public void checkIn(){
+        this.isCheckedOut=false;
+        this.checkedOutTo="";
+
+    }
 }
--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,5 +1,76 @@
 package com.pluralsight;
 
-public class NeighborhoodLibrary {
+import java.util.Scanner;
 
+public class NeighborhoodLibrary {
+    static Book[] books = new Book[20];
+
+    public static void main(String[] args) {
+        books[0] = new Book(1, "", "123456789", "Harry Potter", false);
+        books[1] = new Book(2, "", "987654321", "50 Shades of Grey", false);
+        books[2] = new Book(3, "", "112233445", "Lord of the Rings", false);
+        books[3] = new Book(4, "", "516808956", "Huckleberry Fin", false);
+        books[4] = new Book(5, "", "550563623", "Game of Thrones", false);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1 - Show Available Books");
+            System.out.println("2 - Show Checked Out Books");
+            System.out.println("3 - Exit");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Available Books:");
+                    for (Book book : books) {
+                        if (book != null && !book.isCheckedOut()) {
+                            System.out.println(book.getId() + " - " + book.getTitle() + " (ISBN: " + book.getIsbn() + ")");
+                        }
+                    }
+                    System.out.println("Enter the ID of the book to check out or 0 to return:");
+                    int bookId = scanner.nextInt();
+                    if (bookId != 0) {
+                        System.out.println("Enter your name:");
+                        scanner.nextLine(); // Clear buffer
+                        String name = scanner.nextLine();
+                        for (Book book : books) {
+                            if (book != null && book.getId() == bookId) {
+                                book.checkOut(name);
+                                System.out.println("Book checked out successfully.");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("Checked Out Books:");
+                    for (Book book : books) {
+                        if (book != null && book.isCheckedOut()) {
+                            System.out.println(book.getId() + " - " + book.getTitle() + " checked out to " + book.getCheckedOutTo());
+                        }
+                    }
+                    System.out.println("Enter the ID of the book to check in or 0 to return:");
+                    bookId = scanner.nextInt();
+                    if (bookId != 0) {
+                        for (Book book : books) {
+                            if (book != null && book.getId() == bookId && book.isCheckedOut()) {
+                                book.checkIn();
+                                System.out.println("Book checked in successfully.");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option, please try again.");
+            }
+        }
+        scanner.close();
+    }
 }
